cff22b471e33c726d14c5a6fa46052927f222e9c
rennel <renne.lehto@metropolia.fi>
2025-03-02 20:42:45 +0200

    Kiviarpasekoilu.

---
+++ kiviarpa.py
@@ -0,0 +1,21 @@
+import random
+
+
+
+def kiviarpa(kiviä):
+    if kivet >0:
+        tulos = int = random.randint(0, 1)
+        if tulos == 1:
+            kiviä = kiviä - 1
+
+    return kiviä
+
+
+kivet = 5
+kerätyt_kivet=0
+if kivet > 0:
+    kiviarpa(kivet)
+    print(kivet)
+else:
+    print(kivet)
+    print(kerätyt_kivet)
