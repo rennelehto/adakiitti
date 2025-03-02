643ef0e6ea60a83570f65d4eb70a6db57a8e0608
rennel <renne.lehto@metropolia.fi>
2025-03-02 15:59:35 +0200

    Kenttien arpominen ja listaaminen mantereittain.

---
+++ Kentät per mantere.py
@@ -0,0 +1,166 @@
+import mysql.connector
+import random
+
+#Tähän listaan tulee kentän "Ident" -tunnus, Euroopassa on 118 suurta kenttää.
+lista_kentistäEU=[]
+
+def kenttäkyselyEU():
+    sql = f"select ident from airport, country where airport.iso_country = country.iso_country and country.continent = 'EU' and airport.type = 'large_airport'"
+    #print(sql)
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
+    if kursori.rowcount > 0:
+        for rivi in tulos:
+            lista_kentistäEU.append(rivi[0])
+    return
+
+#Tähän listaan tulee kentän "Ident" -tunnus, Afrikassa on 45 suurta kenttää.
+lista_kentistäAF=[]
+
+def kenttäkyselyAF():
+    sql = f"select ident from airport, country where airport.iso_country = country.iso_country and country.continent = 'AF' and airport.type = 'large_airport'"
+    #print(sql)
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
+    if kursori.rowcount > 0:
+        for rivi in tulos:
+            lista_kentistäAF.append(rivi[0])
+    return
+#Tähän listaan tulee kentän "Ident" -tunnus, Aasiassa on 137 suurta kenttää.
+lista_kentistäAS=[]
+
+def kenttäkyselyAS():
+    sql = f"select ident from airport, country where airport.iso_country = country.iso_country and country.continent = 'AS' and airport.type = 'large_airport'"
+    #print(sql)
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
+    if kursori.rowcount > 0:
+        for rivi in tulos:
+            lista_kentistäAS.append(rivi[0])
+    return
+#Tähän listaan tulee kentän "Ident" -tunnus, Oseaniassa on 17 suurta kenttää.
+lista_kentistäOC=[]
+def kenttäkyselyOC():
+    sql = f"select ident from airport, country where airport.iso_country = country.iso_country and country.continent = 'OC' and airport.type = 'large_airport'"
+    #print(sql)
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
+    if kursori.rowcount > 0:
+        for rivi in tulos:
+            lista_kentistäOC.append(rivi[0])
+    return
+
+#Tähän listaan tulee kentän "Ident" -tunnus, Pohjois-Amerikassa on 112 suurta kenttää.
+lista_kentistäNA=[]
+def kenttäkyselyNA():
+    sql = f"select ident from airport, country where airport.iso_country = country.iso_country and country.continent = 'NA' and airport.type = 'large_airport'"
+    #print(sql)
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
+    if kursori.rowcount > 0:
+        for rivi in tulos:
+            lista_kentistäNA.append(rivi[0])
+    return
+#Tähän listaan tulee kentän "Ident" -tunnus, Etelä-Amerikassa on 22 suurta kenttää.
+lista_kentistäSA=[]
+def kenttäkyselySA():
+    sql = f"select ident from airport, country where airport.iso_country = country.iso_country and country.continent = 'SA' and airport.type = 'large_airport'"
+    #print(sql)
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
+    if kursori.rowcount > 0:
+        for rivi in tulos:
+            lista_kentistäSA.append(rivi[0])
+    return
+
+yhteys = mysql.connector.connect(
+      #  host='127.0.0.1',
+      #  port=3306,
+        database='flight_game',
+        user='python',
+        password='1232',
+        autocommit=True)
+
+kenttäkyselyEU()
+kenttäkyselyAF()
+kenttäkyselyAS()
+kenttäkyselyOC()
+kenttäkyselyNA()
+kenttäkyselySA()
+#print(lista_kentistä)
+
+valitut_kentätEU=[]
+määräEU = 0
+while määräEU < 15:
+    valitut_kentätEU.append(lista_kentistäEU[random.randint(0, 117)])
+    määräEU=määräEU+1
+
+valitut_kentätAF=[]
+määräAF = 0
+while määräAF < 15:
+    valitut_kentätAF.append(lista_kentistäAF[random.randint(0, 44)])
+    määräAF=määräAF+1
+
+valitut_kentätAS=[]
+määräAS = 0
+while määräAS < 15:
+    valitut_kentätAS.append(lista_kentistäAS[random.randint(0, 136)])
+    määräAS=määräAS+1
+
+valitut_kentätOC=[]
+määräOC = 0
+while määräOC < 15:
+    valitut_kentätOC.append(lista_kentistäOC[random.randint(0, 16)])
+    määräOC=määräOC+1
+
+valitut_kentätNA=[]
+määräNA = 0
+while määräNA < 15:
+    valitut_kentätNA.append(lista_kentistäNA[random.randint(0, 111)])
+    määräNA=määräNA+1
+
+valitut_kentätSA=[]
+määräSA = 0
+while määräSA < 15:
+    valitut_kentätSA.append(lista_kentistäNA[random.randint(0, 21)])
+    määräSA=määräSA+1
+
+#print(valitut_kentätEU)
+print(f'✈ Euroopan kenttiä listassa {len(valitut_kentätEU)} kpl.')
+print(f'✈ Afrikan kenttiä listassa {len(valitut_kentätAF)} kpl.')
+print(f'✈ Aasian kenttiä listassa {len(valitut_kentätAS)} kpl.')
+print(f'✈ Oseanian kenttiä listassa {len(valitut_kentätOC)} kpl.')
+print(f'✈ Pohjois-Amerikan kenttiä listassa {len(valitut_kentätNA)} kpl.')
+print(f'✈ Etelä-Amerikan kenttiä listassa {len(valitut_kentätSA)} kpl.')
+
+pelattavat_kentät=[]
+pelattavat_kentät.extend(valitut_kentätEU)
+pelattavat_kentät.extend(valitut_kentätAF)
+pelattavat_kentät.extend(valitut_kentätAS)
+pelattavat_kentät.extend(valitut_kentätOC)
+pelattavat_kentät.extend(valitut_kentätNA)
+pelattavat_kentät.extend(valitut_kentätSA)
+print('✈ ✈ ✈ ✈ ✈')
+print(f'✈ Pelattavia kenttiä {len(pelattavat_kentät)} kpl.')
+print('✈ ✈ ✈ ✈ ✈')
+
+#Tästä eteenpäin kokeilua
+def kenttähaku():
+    sql = f'SELECT airport.name, country.name FROM airport, country WHERE airport.iso_country = country.iso_country and ident = "{pelattavat_kentät[random.randint(0, 89)]}"'
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
+    if kursori.rowcount > 0:
+        for rivi in tulos:
+            print('Airport name: ' + rivi[0] + '.')
+            print('Location: ' + rivi[1] + '.')
+    return
+kenttähaku()
+print('✈ Bon voyage! ✈')
+
