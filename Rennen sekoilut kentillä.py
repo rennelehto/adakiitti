89a7d4e84414874b48babd9d343abc77c928a767
rennel <renne.lehto@metropolia.fi>
2025-03-02 13:13:17 +0200

    Kenttien valinta ja arpominen, testikoodi Suomen kentillä.

--- Rennen sekoilut kentillä.py
+++ Rennen sekoilut kentillä.py
@@ -1,1 +1,34 @@
-#kyl tää tästä
+import mysql.connector
+import random
+
+#Tähän kokeiluun otin Suomen keskikokoiset kentät ja arvoin niistä 10 listalle.
+lista=[]
+def kysely():
+    sql = f"select ident from airport, country where airport.iso_country = country.iso_country and country.iso_country = 'FI' and airport.type = 'medium_airport'"
+   # print(sql)
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
+    if kursori.rowcount > 0:
+        for rivi in tulos:
+            lista.append(rivi)
+    return
+
+yhteys = mysql.connector.connect(
+      #  host='127.0.0.1',
+      #  port=3306,
+        database='flight_game',
+        user='python',
+        password='1232',
+        autocommit=True)
+kysely()
+
+valitut=[]
+määrä = 0
+while määrä < 10:
+    valitut.append(lista[random.randint(0, 29)])
+    määrä=määrä+1
+
+print(valitut)
+print(lista)
+
