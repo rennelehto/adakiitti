5734875f806b85a026063afc4eaf5e56e7384f9c
Fliits <aatuska576@gmail.com>
2025-03-04 15:15:38 +0200

    funktiot siirtymiselle

--- kentät.py
+++ kentät.py
@@ -1,1 +1,13 @@
-print("tämänlainen")
+def siirtyy(kohde):
+    sql = f"update game set location = (select ident from airport where name = {kohde}) where screen_name = 'Pelaaja';"
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    return
+
+def sijainti():
+    sql = f"select name from airport where ident in (select from screen_name where screen_name = 'Pelaaja');"
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
+    print(f"Sijaintisi on {tulos}.")
+    return
