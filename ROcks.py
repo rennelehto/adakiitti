0ff2817c10e5a77dd0705dc67ebe94e02048f6d4
Pinkacai <eliel.j.levine@gmail.com>
2025-03-04 10:17:07 +0200

    Idk

--- ROcks.py
+++ ROcks.py
@@ -1,3 +1,50 @@
-print("MIAU HAU")
-print("'ÄLÄ JAKSAA")
-print("HAHAH")
+print("")
+print("                                                                                         Adakite--Adakiitti")
+print("Tehtäväsi on pelastaa maailma ilkeältä velholta, joka pyrkii keräämään maagisia kiviä joilla hän haluaa aiheuttaa ilmastokatastrofin.")
+Pelin_aloitus = int(input(
+                "\n1. Asia selvä. "
+                "\n2. Okei. "
+                "\n3. En halua. "
+                "\n: "))
+while int(Pelin_aloitus in range(1,4)):
+    if Pelin_aloitus == 1:
+        print("--------------------------------------------------")
+        print("Asia selvä.")
+        print("--------------------------------------------------")
+        break
+    if Pelin_aloitus == 2 :
+        print("--------------------------------------------------")
+        print("Okei.")
+        print("--------------------------------------------------")
+        break
+    if Pelin_aloitus == 3:
+        print("--------------------------------------------------")
+        print("Valitettavasti et saa jatkaa.")
+        print("--------------------------------------------------")
+        quit()
+    else:
+        print("--------------------------------------------------")
+        print("Error,please try again")
+        print("--------------------------------------------------")
+        break
+print("Onnea matkaan, ja käytä voimiasi hyvään.")
+print("--------------------------------------------------")
+print(" Ole hyvä ja valitse lentokenttä josta haluat aloittaa pelin seuraavasta listasta. ")
+Alkupiste = int(input(
+                "\n1. Kenttä 1 "
+                "\n2. Kenttä 2 "
+                "\n3. Kenttä 2 "
+                "\n3. Kenttä 2 "
+                "\n3. Kenttä 2 "
+                "\n3. Kenttä 2 "
+                "\n3. Kenttä 2 "
+                "\n3. Kenttä 2 "
+                "\n3. Kenttä 2 "
+                "\n3. Kenttä 2 "
+                "\n3. Kenttä 2 "
+                "\n: "))
+
+
+
+
+
