38ba128a4b6f440fc9a928530671af4716e4b9d0
Pinkacai <eliel.j.levine@gmail.com>
2025-03-04 12:51:36 +0200

    Idk

--- start_UI.py
+++ start_UI.py
@@ -36,49 +36,23 @@ while int(Pelin_aloitus) == 1 or 2 or 3:
 
 print("Onnea matkaan, ja käytä voimiasi hyvään.")
 print("--------------------------------------------------")
+print(" ")
 print("Ole hyvä ja valitse lentokenttä josta haluat aloittaa pelin seuraavasta listasta. ")
 
 
 
 
 pelattavat_kentät=["Helsinki-Vantaa airport", "JFK international airport", "LAX international airport", "Arlanda airport","London Heathrow airport"]
-pelattavat_kentät2 = ["Helsinki-Vantaa airport", "JFK international airport", "LAX international airport", "Arlanda airport","London Heathrow airport"]
-kenttä1 = []
-kenttä2 = []
-kenttä3 = []
-kenttä4 = []
-kenttä5 = []
-
-for n in range(1):
-    choice = random.choice(pelattavat_kentät2)  # Pick a random element
-    kenttä1.append(choice)
-    pelattavat_kentät2.remove(choice)
-
-    choice = random.choice(pelattavat_kentät2)  # Pick a random element
-    kenttä2.append(choice)
-    pelattavat_kentät2.remove(choice)
-
-    choice = random.choice(pelattavat_kentät2)  # Pick a random element
-    kenttä3.append(choice)
-    pelattavat_kentät2.remove(choice)
-
-    choice = random.choice(pelattavat_kentät2)  # Pick a random element
-    kenttä4.append(choice)
-    pelattavat_kentät2.remove(choice)
-
-    choice = random.choice(pelattavat_kentät2)  # Pick a random element
-    kenttä5.append(choice)
-    pelattavat_kentät2.remove(choice)
 
 
-Alkupiste = int(input(
-                f"\n1. {kenttä1}"
-                f"\n2. {kenttä2}"
-                f"\n3. {kenttä3}"
-                f"\n4. {kenttä4}"
-                f"\n5. {kenttä5}"
-                "\n: "))
 
+numero = 1
+for kenttä in pelattavat_kentät:
+    print(f"{numero}. {kenttä}.")
+    numero = numero + 1
+
+alkupiste = int(input(": "))
+#if alkupiste in pelattavat_kentät:
 
 
 
