6f53aca13ead031b6f66d6cd309c4457344ef578
rennel <renne.lehto@metropolia.fi>
2025-03-07 14:04:02 +0200

    siistin

--- kenttien etäisyyksien mittauskoodi.py
+++ kenttien etäisyyksien mittauskoodi.py
@@ -2,7 +2,7 @@ import mysql.connector
 from geopy import distance
 '''Tässä on mallina vaan väärä kenttälista, saa kokeilla!
 Tähän lisätään kans kertoimena kivien määrä, eli kolme kiveä = 3*500km jne'''
-#TOIMII
+
 #tekee listan pelattavista kentistä
 def pelattavat():
     sql = f"select ident from airport, country where airport.iso_country = country.iso_country and country.iso_country = 'FI' and airport.type = 'medium_airport'"
@@ -10,14 +10,12 @@ def pelattavat():
     kursori = yhteys.cursor()
     kursori.execute(sql)
     tulos = kursori.fetchall()
-    #pelattavat_kentät.append(tulos)
     if kursori.rowcount > 0:
         for rivi in tulos:
             pelattavat_kentät.append(rivi[0])
 
     return tulos
 
-#TOIMII
 #hakee pelaajan koordinaatit
 def pelaajan_sijainti():
     sql = f"SELECT latitude_deg, longitude_deg FROM airport WHERE airport.ident = 'EFHK'"
@@ -30,7 +28,6 @@ def pelaajan_sijainti():
             print(f"Pelaajan  koordinaatit: {rivi[0]}, {rivi[1]}.")
     return tulos
 
-#TOIMII
 #tämä hakee kentän koordinaatit
 def kentän_etäisyys(pylly):
     sql = f"SELECT latitude_deg, longitude_deg from airport, country where airport.iso_country = country.iso_country and ident = '{pylly}'"
@@ -40,8 +37,7 @@ def kentän_etäisyys(pylly):
     tulos = kursori.fetchall()
     return tulos
 
-#TOIMII
-#Jostain syystä pelattavien kenttien eka alkio on koko lista, korjaan myöhemmin
+#etsii kentät joille pelaajalla on pääsy
 def matkustettavat_kentät():
     k=1
     while k <= len(pelattavat_kentät)-1:
@@ -54,6 +50,7 @@ def matkustettavat_kentät():
         k = k + 1
     return
 
+#muuttaa kenttien ICAO-koodit nimiksi
 def koodi_nimeksi(koodi):
     sql = f"SELECT name FROM airport WHERE ident = '{koodi}'"
     # print(sql)
@@ -65,6 +62,7 @@ def koodi_nimeksi(koodi):
             seuraavien_kenttien_nimet.append(rivi[0])
     return tulos
 
+#käy koodit läpi nimenmuutosta varten
 def listaus():
     x = 0
     while x < len(seuraavat_kentät):
@@ -72,17 +70,24 @@ def listaus():
         äh = koodi_nimeksi(n)
         x = x+1
 
+#Luettelee pelaajalle kentät joille voi matkustaa
 def kenttäluettelo():
     x=0
     print('Tässä kentät joille voit matkustaa:')
     print()
     for k in seuraavien_kenttien_nimet:
         print(f'{x+1}.  {k}')
-   # while x < len(seuraavien_kenttien_nimet):
-    #    print(f'{x+1}. {seuraavien_kenttien_nimet[x]}')
-        #print(seuraavien_kenttien_nimet[x])
         x=x+1
     return
+
+#koko kierroksen silmukka
+def uusi_kierros():
+    seuraavat_kentät.clear()
+    seuraavien_kenttien_nimet.clear()
+
+    matkustettavat_kentät()
+    listaus()
+    kenttäluettelo()
 
 yhteys = mysql.connector.connect(
         database='flight_game',
@@ -93,37 +98,32 @@ yhteys = mysql.connector.connect(
 
 
 
-
+#lista listoista hehe
 pelattavat_kentät=[]
 seuraavat_kentät=[]
 seuraavien_kenttien_nimet=[]
 
 sijainti = pelaajan_sijainti()
 print()
+
 pelattavat()
+
 print(f'Pelattavia kenttiä: {len(pelattavat_kentät)}')
+
 matkustettavat_kentät()
+
 print()
 listaus()
 
 print()
 kenttäluettelo()
+
 print()
+
 print(f'Nimettyjä kenttiä: {len(seuraavien_kenttien_nimet)}')
 print(f'Matkustettavia kenttiä: {len(seuraavat_kentät)}')
+
 print()
-
-
-
-
-def uusi_kierros():
-    seuraavat_kentät.clear()
-    seuraavien_kenttien_nimet.clear()
-
-    matkustettavat_kentät()
-    listaus()
-    kenttäluettelo()
-
 
 valinta = input('Seuraava kierros? (kyllä/ei) ')
 
