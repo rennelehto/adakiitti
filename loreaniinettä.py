6063aa015df9ed8ff12561823d6d66484bce4962
Fliits <aatuska576@gmail.com>
2025-03-09 14:42:00 +0200

    lore päivitetty

---
+++ loreaniinettä.py
@@ -0,0 +1,52 @@
+'''
+Ilmasto lämpenee lämpenemistään ja sen estämiseen kykenevät tahot
+vain tahkoavat rahaa ympäristön kustannuksella. On aika tarttua
+toimeen elinympäristön puolustamiseksi!
+
+Ab Ruksi Oy Corporationin toimitusjohtaja velho Suur-Ruksi ylläpitää
+portaaleja ympäri taiattoman maailman lentokenttiä adakiittimanaseoksesta
+louhituilla kivillä, joiden korruptoitunut taikaenergia saastuttaa niin
+lähiympäristöä kuin koko kotiplaneettaa.
+
+Vaan velho Suur-Ruksi on saanut vihiä aikeistasi anastaa kivet ja puhdistaa
+ne saastuttavasta energiasta ennen kuin jaat ne tarvitseville.
+Tästä alkaa kilpajuoksu kivien luo!
+
+Pystyt teleporttaamaan tukikohdastasi yhdelle ennalta määrittelemättömälle
+kentälle, jonka jälkeen sinulla on rajattu määrä energiaa matkustaa portaalien
+välillä. Kerättyäsi adakiitin, et voi enää palata kyseiselle kentälle.
+Voit käyttää myös adakiitteja matkustamiseen, mutta muista että ne kuormittavat
+ympäristöä. Harkitse tarkkaan liikkeesi!
+'''
+
+'''
+Ammoisina aikoina suuret tietäjälahkot ylläpitivät harmoniaa luonnon ja ihmisen välillä. Tietäjät olivat voimakkaita,
+sillä heidän valtansa tuli suoraan jumalilta. Mutta tietäjien ylimielisyys saattoi heidät kauemmaksi ja kauemmaksi
+tehtävästään suojella luontoa ihmiseltä. Tietäjät alkoivat käyttää valtaansa ja voimiaan hallitakseen luontoa ja ihmisiä.
+Kautta historian, tietäjät vaikuttivat maailman kehityksen kulkuun kriittisillä hetkillä, johdattaakseen ihmiskuntaa
+kohti heidän halajamaansa tulevaisuutta, jossa ihmisellä on kaikki valta yli luonnon ja luonnon resurssit ovat täysin
+ihmisen hyppysissä.
+
+Vasta kun tietäjät näkivät teollisuuden aiheuttaman tuhon ihmiselle itselleen, valveutuivat he luontoa riivaavasta
+kriisistä. Mutta teollisuus ja teknologia olivat jo kauan aikaa sitten ylittäneet tietäjien voiman ja resurssit.
+Tietäjillä ei ollut muuta vaihtoehtoa kuin vakuuttaa ihmiskunta uhkaavasta ilmastokatastrofista. Vuosikymmenten työ
+valui kerta toisensa jälkeen hukkaan, sillä ihmiskunnan johtajat eivät suostuneet toimimaan ilmastonlämpenemistä vastaan.
+Suuri pettymys ihmiskunnan kiittämättömyyteen ja ahneuteen jakoi tietäjälahkot kahtia, ja kaksi puolta päättyivät sotaan.
+Vanhat lahkot syyttivät ihmiskuntaa ja halusivat ilmastokatastrofin palauttavan tasapainon luonnon ja ihmisen välille.
+Uudet lahkot taas halusivat estää ilmastokatastrofin ja valistaa ihmiskuntaa heitä uhkaavasta turmiosta.
+
+Sota lahkojen välillä osoittautui mahdottomaksi voittaa, sillä tietäjät olivat liian voimakkaita tuhotakseen toisensa.
+Ennen pitkää, pattitilanne ajoi molemmat puolet kohti yhtä ratkaisua: yhden velhon on kerättävä luonnon voimaa turvaavat
+taikakivet jotka on piilotettu ympäri maailmaa ja käytettävä niiden sisällä olevaa voimaa voittaakseen sota. Vanhojen
+lahkojen suunnitelmana oli käyttää niiden energiaa kiihdyttääkseen ilmastonmuutosta siihen pisteeseen ettei ihmiskunta
+voi millään pysäyttää sitä, samalla kun uudet lahkot halajavat käyttää kyseistä voimaa poistaakseen kaikki taikavoima
+maailmasta, jättäen luonnon kohtalon täysin ihmiskunnalle.
+
+Toteuttaakseen tämän tehtävän, uudet lahkot valitsivat: sinut! Vastustajaksesi, vanhat lahkot valitsivat Suur-Velho 
+Kaik-Oo-Koon, joka halajaa ihmiskunnan tietämättömyyttä ilmastonmuutoksesta. Nyt, sinun kuuluu kerätä niin paljon
+taikakiviä kuin voit käyttämällä maailman lentokenttiä kiintopisteinä ja pysäyttää Kaik-Oo-Koo ennen kuin hän ehtii
+tuhota ilmaston! Muista, että lopullinen tavoite on suojella luontoa, pidä siis huolta että sinun ja vastustajasi kilpa
+ei ehdi tuhota luontoa kokonaan ennen aikojaan!
+
+Onnea matkaan!
+'''
