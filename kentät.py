83e9aa9f99531445ed08768de464f1438067ca83
Fliits <aatuska576@gmail.com>
2025-03-09 13:14:01 +0200

    lisätty vihollisvalinta

--- kentät.py
+++ kentät.py
@@ -1,17 +1,18 @@
+import random
 def siirtyy(kohde):
-    sql = f"update game set location = (select ident from airport where name = {kohde}) where screen_name = 'Pelaaja';"
+    sql = f"update peli set location = (select ident from airport where name = {kohde}) where nimi = 'Pelaaja';"
     kursori = yhteys.cursor()
     kursori.execute(sql)
     return
 
 def sijainti():
-    sql = f"select name from airport where ident in (select from screen_name where screen_name = 'Pelaaja');"
+    sql = f"select name from airport where ident in (select from nimi where nimi = 'Pelaaja');"
     kursori = yhteys.cursor()
     kursori.execute(sql)
     tulos = kursori.fetchall()
     print(f"Sijaintisi on {tulos}.")
     return
 
-alkupiste = int(input(": "))
-
-print(f"Olet kentällä : {valitut_kentätEU[alkupiste-1]}")
+def vihollisvalinta():
+    tulos = random.randint(len(seuraavat_kentät - 1))
+    return tulos
