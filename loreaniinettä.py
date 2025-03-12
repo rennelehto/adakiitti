7c721bb1c9b5a1e590873a00055353f4f09d9fd0
Fliits <aatuska576@gmail.com>
2025-03-12 10:44:53 +0200

    lore tiivistys + lisä

--- loreaniinettä.py
+++ loreaniinettä.py
@@ -20,20 +20,19 @@ ympäristöä. Harkitse tarkkaan liikkeesi!
 '''
 
 '''
-Ammoisina aikoina suuret tietäjälahkot ylläpitivät harmoniaa luonnon ja ihmisen välillä. Tietäjät olivat voimakkaita,
-sillä heidän valtansa tuli suoraan jumalilta. Mutta tietäjien ylimielisyys saattoi heidät kauemmaksi ja kauemmaksi
-tehtävästään suojella luontoa ihmiseltä. Tietäjät alkoivat käyttää valtaansa ja voimiaan hallitakseen luontoa ja ihmisiä.
-Kautta historian, tietäjät vaikuttivat maailman kehityksen kulkuun kriittisillä hetkillä, johdattaakseen ihmiskuntaa
-kohti heidän halajamaansa tulevaisuutta, jossa ihmisellä on kaikki valta yli luonnon ja luonnon resurssit ovat täysin
-ihmisen hyppysissä.
+Ammoisina aikoina suuret tietäjälahkot ylläpitivät harmoniaa luonnon ja ihmisen välillä. Tietäjien suuri voima teki
+heistä ylimielisiä mikä saattoi heidät kauemmaksi ja kauemmaksi tehtävästään suojella luontoa ihmiseltä. Tietäjät
+alkoivat käyttää valtaansa ja voimiaan hallitakseen luontoa ja ihmisiä. Kautta historian, tietäjät vaikuttivat maailman
+kehityksen kulkuun kriittisillä hetkillä, johdattaakseen ihmiskuntaa kohti heidän halajamaansa tulevaisuutta,
+jossa ihmisellä on kaikki valta yli luonnon ja luonnon resurssit ovat täysin ihmisen hyppysissä. Vasta kun tietäjät
+näkivät teollisuuden aiheuttaman tuhon ihmiselle itselleen, valveutuivat he luontoa riivaavasta kriisistä.
 
-Vasta kun tietäjät näkivät teollisuuden aiheuttaman tuhon ihmiselle itselleen, valveutuivat he luontoa riivaavasta
-kriisistä. Mutta teollisuus ja teknologia olivat jo kauan aikaa sitten ylittäneet tietäjien voiman ja resurssit.
-Tietäjillä ei ollut muuta vaihtoehtoa kuin vakuuttaa ihmiskunta uhkaavasta ilmastokatastrofista. Vuosikymmenten työ
-valui kerta toisensa jälkeen hukkaan, sillä ihmiskunnan johtajat eivät suostuneet toimimaan ilmastonlämpenemistä vastaan.
-Suuri pettymys ihmiskunnan kiittämättömyyteen ja ahneuteen jakoi tietäjälahkot kahtia, ja kaksi puolta päättyivät sotaan.
-Vanhat lahkot syyttivät ihmiskuntaa ja halusivat ilmastokatastrofin palauttavan tasapainon luonnon ja ihmisen välille.
-Uudet lahkot taas halusivat estää ilmastokatastrofin ja valistaa ihmiskuntaa heitä uhkaavasta turmiosta.
+Mutta teollisuus ja teknologia olivat jo kauan aikaa sitten ylittäneet tietäjien voiman ja resurssit. Tietäjillä
+ei ollut muuta vaihtoehtoa kuin vakuuttaa ihmiskunta uhkaavasta ilmastokatastrofista. Mutta vuosikymmenten työ valui
+kerta toisensa jälkeen hukkaan, sillä ihmiskunnan johtajat eivät suostuneet toimimaan lainkaan ilmastonmuutoksen
+estämiseksi. Suuri pettymys ihmiskunnan kiittämättömyyteen ja ahneuteen jakoi tietäjälahkot kahtia, ja kaksi puolta
+päättyivät sotaan. Vanhat lahkot syyttivät ihmiskuntaa ja halusivat ilmastokatastrofin palauttavan tasapainon luonnon
+ja ihmisen välille. Uudet lahkot taas halusivat estää ilmastokatastrofin ja valistaa ihmiskuntaa heitä uhkaavasta turmiosta.
 
 Sota lahkojen välillä osoittautui mahdottomaksi voittaa, sillä tietäjät olivat liian voimakkaita tuhotakseen toisensa.
 Ennen pitkää, pattitilanne ajoi molemmat puolet kohti yhtä ratkaisua: yhden velhon on kerättävä luonnon voimaa turvaavat
@@ -42,11 +41,20 @@ lahkojen suunnitelmana oli käyttää niiden energiaa kiihdyttääkseen ilmaston
 voi millään pysäyttää sitä, samalla kun uudet lahkot halajavat käyttää kyseistä voimaa poistaakseen kaikki taikavoima
 maailmasta, jättäen luonnon kohtalon täysin ihmiskunnalle.
 
-Toteuttaakseen tämän tehtävän, uudet lahkot valitsivat: sinut! Vastustajaksesi, vanhat lahkot valitsivat Suur-Velho 
-Kaik-Oo-Koon, joka halajaa ihmiskunnan tietämättömyyttä ilmastonmuutoksesta. Nyt, sinun kuuluu kerätä niin paljon
-taikakiviä kuin voit käyttämällä maailman lentokenttiä kiintopisteinä ja pysäyttää Kaik-Oo-Koo ennen kuin hän ehtii
-tuhota ilmaston! Muista, että lopullinen tavoite on suojella luontoa, pidä siis huolta että sinun ja vastustajasi kilpa
-ei ehdi tuhota luontoa kokonaan ennen aikojaan!
+'''
+'''
+Muinaiset tietäjälahkot ovat sodassa! Vanhat lahkot, joiden tavoite on ilmastonmuutos,
+ovat kaavailleet suunnitelman tuodakseen lopun konfliktille:
+Suur-Velho Kaik-Oo-Koolle on annettu tehtäväksi kerätä kaikki adakiittikivet maailmasta
+voittaakseen velhojen taisto. Uudet lahkot ovat päättäneet pysäyttää heidän aikeensa
+lähettämällä oman valittunsa keräämään kaikki kivet ensin.
+Toteuttaakseen tämän tehtävän, uudet lahkot valitsivat: sinut!
+'''
+input('')
+'''
+Nyt, sinun kuuluu kerätä niin paljon adakiittitaikakiviä kuin voit,
+käyttämällä maailman lentokenttiä kiintopisteinä ja
+pysäyttää Kaik-Oo-Koo ennen kuin hän ehtii tuhota ilmaston!
 
 Onnea matkaan!
 '''
