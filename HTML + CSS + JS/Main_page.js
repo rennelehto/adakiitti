3450e963f89c87a7167077dd51bf58cc7acc43ae
Pinkacai <eliel.j.levine@gmail.com>
2025-04-25 14:19:33 +0300

    Sivu 1 & 2 lisää hommaa, toimiva kartta!!

--- HTML + CSS + JS/Main_page.js
+++ HTML + CSS + JS/Main_page.js
@@ -1,8 +1,31 @@
 'use strict'
 
-let textcontent = document.getElementById("text")
-let textbox = document.getElementById("input")
+var map = L.map('map', {
+    center: [30, 0],        // näyttää koko kartan(melkeen)
+    zoom: 2,
+    minZoom: 2,
+    maxZoom: 2,
+    zoomControl: false,     // Ei voi zoomata komentoja
+    scrollWheelZoom: false,
+    doubleClickZoom: false,
+    boxZoom: false,
+    touchZoom: false,
+    dragging: false          // Kartta ei liiku!
+});
 
-textbox.addEventListener("submit", function(evt){
-  textcontent.textContent = "Here's something new! Yay :)"
-})
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 2,
+    minZoom: 2,
+    attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
+
+L.marker([60.192059, 24.945831]) // Lisää pisteitä kartalle, käyetään tätä jotta saadaan kaikki pisteet kartalle!
+  .addTo(map)
+  .bindPopup('Helsinki-Vantaa airport')
+
+
+L.marker([40.7128, -74.0060]) //
+  .addTo(map)
+  .bindPopup('New York JFK airport')
+
+
