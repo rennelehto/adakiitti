3704c85f1278e479258d44867f85ee6ebbe598a9
Pinkacai <eliel.j.levine@gmail.com>
2025-04-27 19:37:14 +0300

    Sivu 2 lisää hommaa, pisteiden poisto!

--- HTML + CSS + JS/Main_page.js
+++ HTML + CSS + JS/Main_page.js
@@ -1,4 +1,5 @@
 'use strict'
+let button = document.querySelector("button")
 
 var map = L.map('map', {
     center: [30, 0],        // näyttää koko kartan(melkeen)
@@ -19,17 +20,46 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
-function add_to_map(x,y,name){
-   const marker = L.marker([x,y])
-  .addTo(map)
-  .bindPopup(`${name}`)
-  marker.on('mouseover',function(ev) {
-  marker.openPopup();
-});marker.on('mouseout',function(ev) {
-  marker.closePopup();
-});
+const markers = {};  // Antaa nimet pisteille kartalla
+
+function add_to_map(x, y, name) {
+    const marker = L.marker([x, y])
+        .addTo(map)
+        .bindPopup(`${name}`);
+
+    marker.on('mouseover', function(ev) {
+        marker.openPopup();
+    });
+    marker.on('mouseout', function(ev) {
+        marker.closePopup();
+    });
+
+    markers[name] = marker;  // Tallentaa pisteen
+}
+
+function remove_from_map(name) {
+    const marker = markers[name];
+    if (marker) {
+        map.removeLayer(marker);
+        delete markers[name];  // Poistaa pisteen
+    }
 }
 
 // näin lisätään kenttiä!
 add_to_map(40.7128, -74.0060,"New York JFK airport")
 add_to_map(60.192059, 24.945831,"Helsinki-Vantaa airport")
+
+
+let name_of_del = document.getElementById("test")
+
+
+// näin poistetaan kysytty piste; pelkkää demoa, ei tarvita peliin, mutta tästä saa ideoita!
+button.addEventListener("click", function(evt){
+    let name_to_del = name_of_del.value
+  remove_from_map(`${name_to_del}`)
+})
+
+
+
+
+
