0123ef7d3472e3642c29c90f56332bd2f77d7cd2
rennel <renne.lehto@metropolia.fi>
2025-05-02 19:50:19 +0300

    ehhh???

--- DEMO_hehe.py
+++ DEMO_hehe.py
@@ -283,6 +283,7 @@ def peli_alkaa():
     "\n pysäyttää Kaik-Oo-Koo ennen kuin hän ehtii tuhota ilmaston! ")
 
 
+
     Pelin_aloitus = input(                          #Vastaus valinta
                 "\n1. Asia selvä. "
                 "\n2. En halua. "
