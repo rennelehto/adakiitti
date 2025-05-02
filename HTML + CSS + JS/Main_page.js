194b2b0d19fbf567fb5ea12a6385a35ae81aa3d3
Pinkacai <eliel.j.levine@gmail.com>
2025-05-02 14:33:18 +0300

    Hakee tietoa fetchillä, ja laittaa kaikki pisteet mitkä on pelissä kartalle. Demo, auttaa tulevaisuudes mut ei oo loppu pelissä.

--- HTML + CSS + JS/Main_page.js
+++ HTML + CSS + JS/Main_page.js
@@ -58,43 +58,33 @@ function remove_from_map(name) {
       }
     }
   }
+
 }
 let all_airports = {
 
 }
-async function fetchData (){
+async function fetchData () {
   try {
-    const response = await fetch('http://127.0.0.1:3000/airport/')
-    const data = await response.json()
-    console.log(data)
-  } catch(error) {
-    console.log("Error")
+    const response = await fetch('http://127.0.0.1:3000/airport/');
+    const data = await response.json();
+
+    console.log("Fetched airport data:", data);
+
+    all_airports = data;
+
+    data.forEach(airport => {
+      if (airport.lat && airport.long) {
+        add_to_map(airport.lat, airport.long, `${airport.Name} (${airport.ICAO})`);
+      }
+    });
+
+  } catch (error) {
+    console.error("Error fetching data", error);
   }
 }
-
 fetchData()
 
-// näin lisätään kenttiä!
-add_to_map(40.7128, -74.0060, 'New York JFK airport');
-add_to_map(60.192059, 24.945831, 'Helsinki-Vantaa airport');
-add_to_map(33.6407, -84.4277, 'Atlanta Hartsfield–Jackson Intl');
-add_to_map(40.0801, 116.5846, 'Beijing Capital Intl');
-add_to_map(25.2532, 55.3657, 'Dubai Intl');
-add_to_map(33.9416, -118.4085, 'Los Angeles Intl');
-add_to_map(35.5494, 139.7798, 'Tokyo Haneda Intl');
-add_to_map(41.9742, -87.9073, "Chicago O'Hare Intl");
-add_to_map(51.4700, -0.4543, 'London Heathrow');
-add_to_map(22.3080, 113.9185, 'Hong Kong Intl');
-add_to_map(31.1443, 121.8083, 'Shanghai Pudong Intl');
-add_to_map(49.0097, 2.5479, 'Paris Charles de Gaulle');
-add_to_map(52.3105, 4.7683, 'Amsterdam Schiphol');
-add_to_map(50.0379, 8.5622, 'Frankfurt Intl');
-add_to_map(32.8998, -97.0403, 'Dallas/Fort Worth Intl');
-add_to_map(23.3924, 113.2988, 'Guangzhou Baiyun Intl');
-add_to_map(1.3644, 103.9915, 'Singapore Changi');
-add_to_map(37.4602, 126.4407, 'Seoul Incheon Intl');
-add_to_map(39.8561, -104.6737, 'Denver Intl');
-add_to_map(41.2753, 28.7519, 'Istanbul Airport');
+
 
 let name_of_del = document.getElementById('test');
 
