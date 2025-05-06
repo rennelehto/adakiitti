c92247013c4d9e843488b2f654800eeac13dcb32
rennel <renne.lehto@metropolia.fi>
2025-05-06 21:20:13 +0300

    etusivu

--- Highscore/Game_start_page_JS.js
+++ Highscore/Game_start_page_JS.js
@@ -4,9 +4,7 @@ let button1 = document.querySelector("#startbutton")
 button1.style.fontSize = "xx-large"
 button1.addEventListener("click", (event) =>{
   location.href="Main_page.html"
-
 })
-
 let button2 = document.querySelector("#hsbutton")
 button2.style.fontSize = "xx-large"
 button2.addEventListener("click", (event) =>{
