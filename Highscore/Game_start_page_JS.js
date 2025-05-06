edef9ca65bdebaa7f3e5c0ebdeed7131f05c673e
rennel <renne.lehto@metropolia.fi>
2025-05-06 21:15:35 +0300

    6.5.

--- Highscore/Game_start_page_JS.js
+++ Highscore/Game_start_page_JS.js
@@ -1,9 +1,16 @@
 'use strict'
 
-let button = document.querySelector("#button")
-button.style.fontSize = "xx-large"
-button.addEventListener("click", (event) =>{
+let button1 = document.querySelector("#startbutton")
+button1.style.fontSize = "xx-large"
+button1.addEventListener("click", (event) =>{
   location.href="Main_page.html"
+
+})
+
+let button2 = document.querySelector("#hsbutton")
+button2.style.fontSize = "xx-large"
+button2.addEventListener("click", (event) =>{
+  location.href="highscore.html"
 
 })
 
